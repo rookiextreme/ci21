9f965fb012e4c4fa7b22c1bd43e059a14c2c4b9c
rubmin <rubmin@vn.net.my>
2021-11-18 12:01:37 +0800

    nullable, migratio edit

--- database/migrations/2021_09_23_023722_create_dict_bank_jobgroup_sets_table.php
+++ database/migrations/2021_09_23_023722_create_dict_bank_jobgroup_sets_table.php
@@ -15,24 +15,24 @@ class CreateDictBankJobgroupSetsTable extends Migration
     {
         Schema::create('dict_bank_jobgroup_sets', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('profiles_id');
-            $table->integer('years_id');
-            $table->integer('jurusan_id');
+            // $table->integer('profiles_id');
+            // $table->integer('years_id');
+            $table->text('jurusan_id');
             $table->integer('dict_bank_grades_categories_id');
             $table->integer('dict_bank_sets_id');
             $table->text('title_eng');
-            $table->text('title_mal');
-            $table->text('desc_eng');
-            $table->text('desc_mal');
-            $table->integer('ref_id');
+            $table->text('title_mal')->nullable();
+            $table->text('desc_eng')->nullable();;
+            $table->text('desc_mal')->nullable();;
+            // $table->integer('ref_id');
             $table->integer('flag');
             $table->integer('delete_id');
             $table->timestamps();
         });
 
         Schema::table('dict_bank_jobgroup_sets', function(Blueprint $table){
-            $table->foreign('profiles_id')->references('id')->on('profiles');
-            $table->foreign('years_id')->references('id')->on('years');
+            // $table->foreign('profiles_id')->references('id')->on('profiles');
+            // $table->foreign('years_id')->references('id')->on('years');
             $table->foreign('dict_bank_grades_categories_id')->references('id')->on('dict_bank_grades_categories');
             $table->foreign('dict_bank_sets_id')->references('id')->on('dict_bank_sets');
         });
